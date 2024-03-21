834357d30261f706c30d86135c086544f05b8d0a
Lem4aks <49612918+Lem4aks@users.noreply.github.com>
2024-03-21 17:23:33 +0200

    Add files via upload
    add better car controll

---
+++ Assets/Scripts/Cars/carinput.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+
+public class CarUserControl : MonoBehaviour
+{
+    private CarController m_Car;
+
+
+    private void Awake()
+    {
+        m_Car = GetComponent<CarController>();
+    }
+
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float handbrake = Input.GetAxis("Jump");
+        m_Car.Move(h, v, v, handbrake);
+    }
+}
